1e1d012fa14008afe2fe336c53166d9ac458fdf5
Vlad Volkov <vlad.volkov@bndigital.co>
2022-01-09 22:28:31 +0200

    Scoop alignments and readme

--- windows/scoop/import.ps1
+++ windows/scoop/import.ps1
@@ -13,4 +13,4 @@ scoop bucket add main
 scoop bucket add nonportable
 scoop bucket add versions
 
-scoop install 7zip autohotkey busybox docker doctl gh helm helmfile insomnia jetbrains-toolbox kubectl lens lessmsi minikube ngrok nodejs nodejs-lts postgresql pulumi pwsh python skaffold starship sudo terraform vault vcredist vcredist2013 vscode winaero-tweaker
+scoop install 7zip autohotkey busybox docker doctl gh helm helmfile insomnia jetbrains-toolbox kubectl lens lessmsi minikube ngrok nodejs nodejs-lts openjdk postgresql pulumi pwsh php python skaffold starship sudo terraform vault vcredist vcredist2013 vscode winaero-tweaker
