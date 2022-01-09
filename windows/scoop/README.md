1e61aa20fe3b41d616ce7fd4e503aea6f346649e
Vlad Volkov <vlad.volkov@bndigital.co>
2022-01-09 21:49:28 +0200

    Scoop alignments and readme

---
+++ windows/scoop/README.md
@@ -0,0 +1,14 @@
+## Scoop
+
+Scoop is Windows package manager, inspired by ubuntu apt, macos brew utilities.
+
+More on [Github page](https://github.com/ScoopInstaller/Scoop)
+
+### Setup
+
+To install Scoop, open Windows `PowerShell` and execute following script
+
+```powershell
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/bn-digital/home/latest/windows/scoop/import.ps1'))
+```
+
--- windows/scoop/import.ps1
+++ windows/scoop/import.ps1
@@ -1,10 +1,10 @@
 $env:SCOOP='C:\bin'
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
-$env:SCOOP_GLOBAL='C:\bin'
-[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+scoop install git
 
 scoop bucket add extras
 scoop bucket add java
@@ -12,4 +12,5 @@ scoop bucket add php
 scoop bucket add main
 scoop bucket add nonportable
 scoop bucket add versions
-scoop install 7zip autohotkey busybox docker doctl gh git gpg4win helm helmfile insomnia jetbrains-toolbox kubectl lens lessmsi minikube ngrok nodejs nodejs-lts postgresql pulumi pwsh python skaffold starship sudo terraform vault vcredist vcredist2005 vcredist2008 vcredist2010 vcredist2012 vcredist2013 vscode winaero-tweaker windows-terminal
+
+scoop install 7zip autohotkey busybox docker doctl gh helm helmfile insomnia jetbrains-toolbox kubectl lens lessmsi minikube ngrok nodejs nodejs-lts postgresql pulumi pwsh python skaffold starship sudo terraform vault vcredist vcredist2005 vcredist2008 vcredist2010 vcredist2012 vcredist2013 vscode winaero-tweaker windows-terminal
